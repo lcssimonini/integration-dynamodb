87f9ddf583553e776034689eefdfdb41c84477cf
Vinicius Faria de Souza <vinicius.faria@cvortex.io>
2020-07-03 14:30:44 -0300

    Fist commmit

---
+++ src/main/java/br/com/vfs/integrationdynamodb/IntegrationDynamodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.vfs.integrationdynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationDynamodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationDynamodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vfs/integrationdynamodb/IntegrationDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vfs.integrationdynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationDynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
