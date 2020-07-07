b77e54e5cd828b7af56a8f51bfc8150fde181c2f
Vinicius Faria de Souza <vinicius.faria@cvortex.io>
2020-07-06 22:14:11 -0300

    colocando configuracoes no profile correto

--- src/main/java/br/com/vfs/integrationdynamodb/config/DynamoDBConfigAws.java
+++ src/main/java/br/com/vfs/integrationdynamodb/config/DynamoDBConfigAws.java
@@ -2,8 +2,8 @@ package br.com.vfs.integrationdynamodb.config;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-//import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
-//import org.springframework.beans.factory.annotation.Value;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,7 +12,6 @@ import static com.amazonaws.regions.Regions.US_EAST_1;
 
 @Profile("aws")
 @Configuration
-//@EnableDynamoDBRepositories(basePackages = "br.com.vfs.integrationdynamodb.repository")
 public class DynamoDBConfigAws {
 
     @Bean
@@ -22,4 +21,19 @@ public class DynamoDBConfigAws {
                 .withRegion(US_EAST_1)
                 .build();
     }
+
+    @Bean
+    public DynamoDBMapper mapper(){
+        return new DynamoDBMapper(amazonDynamoDB(), dynamoDBMapperConfig());
+    }
+
+    private DynamoDBMapperConfig dynamoDBMapperConfig(){
+        return DynamoDBMapperConfig.builder()
+//                .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.CLOBBER)
+//                .withConsistentReads(DynamoDBMapperConfig.ConsistentReads.CONSISTENT)
+//                .withTableNameOverride(null)
+//                .withPaginationLoadingStrategy(DynamoDBMapperConfig.PaginationLoadingStrategy.EAGER_LOADING)
+                .build();
+    }
+
 }
